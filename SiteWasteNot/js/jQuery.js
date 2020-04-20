a981b16850094d7a5f16170644f59790083f574a
Livia Melo Alves de Morais <56313826+liviamelodev@users.noreply.github.com>
2020-04-20 13:58:03 -0300

    Add files via upload

---
+++ SiteWasteNot/js/jQuery.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	$(".card1").hide();
+
+	$("#larissa").mouseover(function () {
+		$("#card1").fadeIn();
+	});
+
+	$("#larissa").mouseout(function () {
+		$(".card1").hide();
+	});
+});
+
+$(document).ready(function(){
+	$(".card2").hide();
+
+	$("#lavinia").mouseover(function () {
+		$("#card2").fadeIn();
+	});
+
+	$("#lavinia").mouseout(function () {
+		$("#card2").hide();
+	});
+});
+$(document).ready(function(){
+	$(".card3").hide();
+
+	$("#livia").mouseover(function () {
+		$("#card3").fadeIn();
+	});
+
+	$("#livia").mouseout(function () {
+		$(".card3").hide();
+	});
+});
+$(document).ready(function(){
+	$(".card4").hide();
+
+	$("#lohayne").mouseover(function () {
+		$("#card4").fadeIn();
+	});
+
+	$("#lohayne").mouseout(function () {
+		$(".card4").hide();
+	});
+});
+$(document).ready(function(){
+	$(".card5").hide();
+
+	$("#vanessa").mouseover(function () {
+		$("#card5").fadeIn();
+	});
+
+	$("#vanessa").mouseout(function () {
+		$(".card5").hide();
+	});
+});
+
+
+
